982a6e3c0640c0b287e4fd72970347a5b4d7601a
Axel Schneider <axel99092@t-online.de>
2020-12-26 17:30:04 +0100

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
+echo "Ordner erstellen falls nicht vorhanden"
+mkdir -p ~/.local/share/Gewicht
 echo "temporäres Backup anlegen"
 [ -e /tmp/backupG ] && rm -rf /tmp/backupG
 mkdir /tmp/backupG
 cp ~/.local/share/Gewicht/*.gnuplot /tmp/backupG
 cp ~/.local/share/Gewicht/*.csv /tmp/backupG
 cp ~/.local/share/Gewicht/*.png /tmp/backupG
-echo "altes Programm löschen"
-[ -e ~/.local/share/Gewicht ] && rm -rf ~/.local/share/Gewicht
 cd ~/Downloads
 echo "alte main.zip löschen"
 [ -e main.zip ] && rm main.zip
 echo "herunterladen ..."
 wget https://github.com/Axel-Erfurt/Gewicht/archive/main.zip
 echo "entpacken ..."
-unzip main.zip
-echo "verschiebe nach ~/local/share"
-mv Gewicht-main ~/.local/share/Gewicht
+unzip -o -j main.zip -d ~/.local/share/Gewicht
 echo "kopiere Icon nach .icons"
 cp ~/.local/share/Gewicht/waage.png ~/.icons
 echo "kopiere Starter nach ~/.local/share/applications"
