28bd56e4af13bbd58c9038900a9e25e10a173bac
Axel Schneider <axel99092@t-online.de>
2020-12-26 16:02:36 +0100

    Update Installer.sh
    angepasst

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+echo "alte csv und preview kopieren"
+[ -e ~/.local/share/Gewicht/Gewicht.csv ] && cp ~/.local/share/Gewicht/Gewicht.csv /tmp/Gewicht.csv
+[-e ~/.local/share/Gewicht/preview_extern.gnuplot] && cp ~/.local/share/Gewicht/preview_extern.gnuplot /tmp/preview_extern.gnuplot
+[-e ~/.local/share/Gewicht/preview_intern.gnuplot] && cp ~/.local/share/Gewicht/preview_intern.gnuplot /tmp/Gewicht_preview_intern.gnuplot
+echo "altes Programm löschen"
+[ -e ~/.local/share/Gewicht ] && rm -rf ~/.local/share/Gewicht
 cd ~/Downloads
-rm ./main.zip
+echo "alte main.zip löschen"
+[ -e main.zip ] && rm main.zip
 echo "herunterladen ..."
 wget https://github.com/Axel-Erfurt/Gewicht/archive/main.zip
 echo "entpacken ..."
@@ -11,5 +18,9 @@ echo "kopiere Icon nach .icons"
 cp ~/.local/share/Gewicht/waage.png ~/.icons
 echo "kopiere Starter nach ~/.local/share/applications"
 cp ~/.local/share/Gewicht/Gewicht.desktop ~/.local/share/applications
+echo "alte csv und preview zurückkopieren"
+[ -e /tmp/Gewicht.csv ] && mv /tmp/Gewicht.csv ~/.local/share/Gewicht/Gewicht.csv 
+[ -e /tmp/preview_extern.gnuplot ] && mv /tmp/preview_extern.gnuplot ~/.local/share/Gewicht/preview_extern.gnuplot
+[ -e /tmp/preview_extern.gnuplot ] && mv /tmp/Gewicht_preview_intern.gnuplot ~/.local/share/Gewicht/preview_intern.gnuplot
 echo "main.zip löschen"
-rm ./main.zip
+[ -e main.zip ] && rm main.zip
